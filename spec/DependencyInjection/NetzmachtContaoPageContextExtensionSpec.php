97cdfcad3540b2721ca1b04f966c75cea694b3ff
David Molineus <david.molineus@netzmacht.de>
2019-11-25 07:59:58 +0100

    Fix spec for current dependency container implementations
    ContainerBuilding#removeBindings is called in later implementations

--- spec/DependencyInjection/NetzmachtContaoPageContextExtensionSpec.php
+++ spec/DependencyInjection/NetzmachtContaoPageContextExtensionSpec.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 
 namespace spec\Netzmacht\Contao\PageContext\DependencyInjection;
 
-use function dirname;
 use Netzmacht\Contao\PageContext\DependencyInjection\NetzmachtContaoPageContextExtension;
 use Netzmacht\Contao\PageContext\EventListener\PageContextListener;
 use Netzmacht\Contao\PageContext\Request\PageContextFactory;
@@ -25,6 +24,8 @@ use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
+use function dirname;
+use function method_exists;
 
 final class NetzmachtContaoPageContextExtensionSpec extends ObjectBehavior
 {
@@ -46,6 +47,10 @@ final class NetzmachtContaoPageContextExtensionSpec extends ObjectBehavior
             ->willReturn(true);
 
         $container->hasExtension("http://symfony.com/schema/dic/services")->willReturn(false);
+
+        if (method_exists($container->getWrappedObject(), 'removeBindings')) {
+            $container->removeBindings(Argument::type('string'))->shouldBeCalled();
+        }
 
         $container->setDefinition(PageIdDeterminator::class, Argument::type(Definition::class))->shouldBeCalled();
         $container->setDefinition(PageContextFactory::class, Argument::type(Definition::class))->shouldBeCalled();
